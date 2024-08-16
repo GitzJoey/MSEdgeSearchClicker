1b535cfaa795e8795cf0f7ac9f96f0a444925d5a
GitzJoey <gitzjoey@live.com>
2024-08-16 15:49:08 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ const main = () => {
   const sec8IntervalDiv = document.getElementById('8SecInterval') as HTMLDivElement;
   const counterDiv = document.getElementById('counter') as HTMLDivElement;
 
-  const MAX_8_SECOND_RUNS = 3; // Number of times the 8-second interval should run in each main interval
-  const MAX_MAIN_INTERVAL_RUNS = 35; // Total number of searches to be performed
+  const MAX_8_SECOND_RUNS = 3;
+  const MAX_MAIN_INTERVAL_RUNS = 35;
   let counter = 0;
   let intervalId: number;
   let mainIntervalId: number;
@@ -19,7 +19,6 @@ const main = () => {
   };
 
   const randomText = () => {
-    // Generate a random number of words between 1 and 10
     const wordCount = Math.floor(Math.random() * 10) + 1;
     return randomWords(wordCount).join(' ');
   };
@@ -30,8 +29,7 @@ const main = () => {
     counter++;
     updateDiv();
 
-    // Stop the intervals if counter reaches MAX_MAIN_INTERVAL_RUNS
-    if (counter === MAX_MAIN_INTERVAL_RUNS) {
+    if (counter >= MAX_MAIN_INTERVAL_RUNS) {
       clearInterval(intervalId);
       clearInterval(mainIntervalId);
       clearInterval(countdownIntervalId);
@@ -45,7 +43,7 @@ const main = () => {
     let innerCounter = 0;
 
     intervalId = setInterval(() => {
-      if (innerCounter < MAX_8_SECOND_RUNS) {
+      if (innerCounter < MAX_8_SECOND_RUNS && counter < MAX_MAIN_INTERVAL_RUNS) {
         func();
         innerCounter++;
         sec8IntervalDiv.innerText = `8-Second Interval: Running... (${innerCounter}/${MAX_8_SECOND_RUNS})`;
@@ -56,7 +54,6 @@ const main = () => {
     }, 8000);
   };
 
-  // Start the countdown
   const startCountdown = () => {
     countdownValue = 30;
     mainIntervalDiv.innerText = `Main Interval (30 sec): ${countdownValue} seconds remaining`;
@@ -71,7 +68,6 @@ const main = () => {
     }, 1000);
   };
 
-  // Start the 30-second interval
   mainIntervalId = setInterval(() => {
     if (counter < MAX_MAIN_INTERVAL_RUNS) {
       start8SecondInterval();
@@ -82,13 +78,12 @@ const main = () => {
     }
   }, 30000);
 
-  // Initial state
   mainIntervalDiv.innerText = `Main Interval (30 sec): ${countdownValue} seconds remaining`;
   sec8IntervalDiv.innerText = `8-Second Interval: Waiting...`;
   counterDiv.innerText = `Counter: ${counter}`;
 
-  // Start the first countdown immediately
   startCountdown();
+  start8SecondInterval();
 };
 
 main();
