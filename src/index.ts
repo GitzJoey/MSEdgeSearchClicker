65563269fd57fdad0a94a004fca5c32fac3a5202
GitzJoey <gitzjoey@live.com>
2024-08-16 15:27:08 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,23 @@ const main = () => {
   const sec8IntervalDiv = document.getElementById('8SecInterval') as HTMLDivElement;
   const counterDiv = document.getElementById('counter') as HTMLDivElement;
 
+  const MAX_8_SECOND_RUNS = 3; // Number of times the 8-second interval should run in each main interval
+  const MAX_MAIN_INTERVAL_RUNS = 35; // Total number of searches to be performed
   let counter = 0;
   let intervalId: number;
   let mainIntervalId: number;
+  let countdownIntervalId: number;
+  let countdownValue = 30;
 
   const updateDiv = () => {
-    mainIntervalDiv.innerText = `Main Interval (30 sec): Running...`;
-    sec8IntervalDiv.innerText = `8-Second Interval: Running...`;
     counterDiv.innerText = `Counter: ${counter}`;
   };
 
   const randomText = () => {
     // Generate a random number of words between 1 and 10
-    const wordCount = Math.floor(Math.random() * 10) + 1; 
+    const wordCount = Math.floor(Math.random() * 10) + 1;
     return randomWords(wordCount).join(' ');
-  }
+  };
 
   const func = () => {
     const searchString = randomText();
@@ -28,35 +30,52 @@ const main = () => {
     counter++;
     updateDiv();
 
-    // Stop the intervals if counter reaches 35
-    if (counter === 35) {
+    // Stop the intervals if counter reaches MAX_MAIN_INTERVAL_RUNS
+    if (counter === MAX_MAIN_INTERVAL_RUNS) {
       clearInterval(intervalId);
       clearInterval(mainIntervalId);
+      clearInterval(countdownIntervalId);
       mainIntervalDiv.innerText = `Main Interval (30 sec): Stopped`;
       sec8IntervalDiv.innerText = `8-Second Interval: Stopped`;
       counterDiv.innerText = `Counter: ${counter} - Reached limit`;
     }
-  }
+  };
 
   const start8SecondInterval = () => {
     let innerCounter = 0;
 
     intervalId = setInterval(() => {
-      if (innerCounter < 3) {
+      if (innerCounter < MAX_8_SECOND_RUNS) {
         func();
         innerCounter++;
-        sec8IntervalDiv.innerText = `8-Second Interval: Running... (${innerCounter}/3)`;
+        sec8IntervalDiv.innerText = `8-Second Interval: Running... (${innerCounter}/${MAX_8_SECOND_RUNS})`;
       } else {
         clearInterval(intervalId);
         sec8IntervalDiv.innerText = `8-Second Interval: Paused`;
       }
     }, 8000);
-  }
+  };
+
+  // Start the countdown
+  const startCountdown = () => {
+    countdownValue = 30;
+    mainIntervalDiv.innerText = `Main Interval (30 sec): ${countdownValue} seconds remaining`;
+
+    countdownIntervalId = setInterval(() => {
+      countdownValue--;
+      mainIntervalDiv.innerText = `Main Interval (30 sec): ${countdownValue} seconds remaining`;
+
+      if (countdownValue === 0) {
+        clearInterval(countdownIntervalId);
+      }
+    }, 1000);
+  };
 
   // Start the 30-second interval
   mainIntervalId = setInterval(() => {
-    if (counter < 35) {
+    if (counter < MAX_MAIN_INTERVAL_RUNS) {
       start8SecondInterval();
+      startCountdown();
     } else {
       clearInterval(mainIntervalId);
       mainIntervalDiv.innerText = `Main Interval (30 sec): Stopped`;
@@ -64,9 +83,12 @@ const main = () => {
   }, 30000);
 
   // Initial state
-  mainIntervalDiv.innerText = `Main Interval (30 sec): Waiting...`;
+  mainIntervalDiv.innerText = `Main Interval (30 sec): ${countdownValue} seconds remaining`;
   sec8IntervalDiv.innerText = `8-Second Interval: Waiting...`;
   counterDiv.innerText = `Counter: ${counter}`;
-}
+
+  // Start the first countdown immediately
+  startCountdown();
+};
 
 main();
